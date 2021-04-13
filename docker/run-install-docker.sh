846a819a6c45795505ab1375137ee2917ecac9a8
BigRamy#0318 <john.kolahdouzan@gmail.com>
2021-04-13 13:29:25 -0400

    refactor

--- docker/run-install-docker.sh
+++ docker/run-install-docker.sh
@@ -1,5 +1,14 @@
 
 
+# dpkg -l | grep -i docker
+# sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli
+# sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce  
+# sudo rm -rf /var/lib/docker /etc/docker
+# sudo rm /etc/apparmor.d/docker
+# sudo groupdel docker
+# sudo rm -rf /var/run/docker.sock
+
+
 sudo docker container stop $(sudo docker container ls -aq)
 sudo docker system prune --volumes
 sudo rm -rf /var/lib/docker
--- docker/run.sh
+++ docker/run.sh
@@ -2,6 +2,23 @@
 
 echo "Hello World!"
 
+# sudo minikube stop; sudo minikube delete &&
+# docker stop $(docker ps -aq) &&
+# docker system prune -f --volumes &&
+# sudo rm -rf ~/.kube ~/.minikube &&
+# sudo rm -rf /usr/local/bin/localkube /usr/local/bin/minikube &&
+# sudo systemctl stop '*kubelet*.mount' &&
+# sudo systemctl stop localkube.service &&
+# sudo systemctl disable localkube.service &&
+# sudo rm -rf /etc/kubernetes/
+
+
+
+#  curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+#  sudo install minikube-linux-amd64 /usr/local/bin/minikube
+# minikube kubectl -- get po -A
+
+
 minikube delete --all
 
 sudo apt-get update
